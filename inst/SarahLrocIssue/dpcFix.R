8a68b331ac8dbbc43cbf1194799c0bc4b5b02aa5
dpc10ster <dpc10ster@gmail.com>
2025-04-19 13:56:33 -0400

    Delete dpcFix.R

--- inst/SarahLrocIssue/dpcFix.R
+++
@@ -1,9 +0,0 @@
-library(RJafroc)
-
-fn <- system.file("extdata", "/toyFiles/LROC/lroc.xlsx", package = "RJafroc", mustWork = TRUE)
-
-lrocData <- DfReadDataFile(fn, newExcelFileFormat = TRUE, lrocForcedMark = TRUE)
-
-p <- PlotEmpOpChrs(lrocData, opChType = "LROC")
-
-print(p)
